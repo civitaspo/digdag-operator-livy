6d08740bfea6cd64fd08d4a770d0f2327ed1f66a
Civitaspo <civitaspo@gmail.com>
2018-08-03 08:23:56 +0900

    Get params for a job

--- src/main/scala/pro/civitaspo/digdag/plugin/livy/operator/LivySubmitJobOperator.scala
+++ src/main/scala/pro/civitaspo/digdag/plugin/livy/operator/LivySubmitJobOperator.scala
@@ -1,9 +1,12 @@
 package pro.civitaspo.digdag.plugin.livy.operator
 
+import com.google.common.base.Optional
 import io.digdag.client.config.Config
 import io.digdag.spi.{OperatorContext, TaskResult, TemplateEngine}
 import io.digdag.standards.operator.state.TaskState
 import io.digdag.util.DurationParam
+
+import scala.collection.JavaConverters._
 
 class LivySubmitJobOperator(context: OperatorContext, systemConfig: Config, templateEngine: TemplateEngine)
   extends AbstractLivyOperator(context, systemConfig, templateEngine) {
@@ -15,5 +18,26 @@ class LivySubmitJobOperator(context: OperatorContext, systemConfig: Config, temp
 
   override def runTask(): TaskResult = {
     null
+  }
+
+  protected def submitJob = {
+    val file: String = job.get("file", classOf[String])
+    val proxyUser: Optional[String] = job.getOptional("proxy_user", classOf[String])
+    val className: Optional[String] = job.getOptional("class_name", classOf[String])
+    val args: Seq[String] = job.getListOrEmpty("args", classOf[String]).asScala
+    val jars: Seq[String] = job.getListOrEmpty("jars", classOf[String]).asScala
+    val pyFiles: Seq[String] = job.getListOrEmpty("py_files", classOf[String]).asScala
+    val files: Seq[String] = job.getListOrEmpty("files", classOf[String]).asScala
+    val driverMemory: Optional[String] = job.getOptional("driver_memory", classOf[String])
+    val driverCores: Optional[Int] = job.getOptional("driver_cores", classOf[Int])
+    val executorMemory: Optional[String] = job.getOptional("executor_memory", classOf[String])
+    val executorCores: Optional[Int] = job.getOptional("executor_cores", classOf[Int])
+    val numExecutors: Optional[Int] = job.getOptional("num_executors", classOf[Int])
+    val archives: Seq[String] = job.getListOrEmpty("archives", classOf[String]).asScala
+    val queue: Optional[String] = job.getOptional("queue", classOf[String])
+    val name: String = job.get("name", classOf[String], s"digdag-${params.get("session_uuid", classOf[String])}")
+    val conf: Map[String, String] = params.getMapOrEmpty("conf", classOf[String], classOf[String]).asScala.toMap
+
+
   }
 }
